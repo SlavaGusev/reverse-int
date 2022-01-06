1e1704b9ef0ac78331c8246244088d3332fc89b1
SlavaGusev <guseff.slawa84@gmail.com>
2022-01-06 07:46:12 +0300

    :fix changes in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+	let result = 0;
+	if (n < 0) {
+		n = -n;
+	}
+	while (n > 0) {
+		result = result + n % 10;
+		n = Math.floor(n / 10);
+		if (n > 0) 
+			result *= 10;
+	}	
+	return result;  
 }
